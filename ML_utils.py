af77af4512358373c52eaf95cb43eea3bbcef76e
Alefe de Oliveira Freire de Almeida <alefe@ibm.com>
2019-12-24 15:11:51 -0300

    insertion of cv for regression in ML_utils.py

--- ML_utils.py
+++ ML_utils.py
@@ -1,9 +1,22 @@
 from sklearn.model_selection import KFold,StratifiedKFold
 from sklearn.metrics import f1_score,accuracy_score,precision_score,recall_score,fbeta_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
 
 class Cross_valid_clf():
-    
+  """ 
+    This class does cross validation for general classifiers. 
+        model: Sklearn model or customized model with fit and predict methods;
+        X: features
+        y:target
+        folds: Number of folds;
+        metrics : accuracy,f1score, precision,recall,fbeta score;
+        stratified: Use stratified Kfold to keep the ratio of classes in all folds;
+        beta: Beta parameter for fbeta score metric;
+        verbose: Flag to print report over iterations;
+        
+    returns: List with scores over the folders
+  """            
   def __init__(self, X, y,n_splits,stratified=True):
     self.n_splits = n_splits
     self.X = X
@@ -99,4 +112,67 @@ class Cross_valid_clf():
         if verbose:
             print(f'\n Overall Score:')
             print(f'fbeta_score Mean: {np.mean(fbetascore)}   Std: {np.std(fbetascore)}')
-        return np.mean(fbetascore)  
+        return np.mean(fbetascore)
+
+class Cross_valid_reg():
+  """ 
+    This class does cross validation for general regressors. 
+        model: Sklearn model or customized model with fit and predict methods;
+        x : features;
+        y: target
+        folds: Number of folds;
+        metrics : RMSE =root mean squared error; MAE= mean absolute error
+        stratified: Use stratified Kfold to keep the ratio of classes in all folds;
+        verbose: Flag to print report over iterations;
+        
+    returns: List with scores over the folders
+  """    
+  def __init__(self, X, y,n_splits,stratified=True):
+    self.n_splits = n_splits
+    self.X = X
+    self.y = y
+        
+    if stratified:
+        self.kf=StratifiedKFold(self.n_splits,shuffle=False,random_state=0)
+        self.kf.get_n_splits(self.X)
+    else:
+        self.kf=KFold(self.n_splits,shuffle=False,random_state=0)
+        self.kf.get_n_splits(self.X)
+
+  #score method
+  def rmse(self, reg,verbose=True):
+    #rmse
+        rmse = []
+        i=0
+        for tr, te in self.kf.split(self.X,self.y):
+            reg.fit(self.X[tr],self.y[tr])
+            y_pred=reg.predict(self.X[te])
+            rmse.append(mean_squared_error(y_pred,self.y[te]))
+            if verbose:
+                print('-'*30)
+                print(f'\nFold {i+1} out of {self.n_splits}')
+                print(f'RMSE: {rmse[i]}')
+            i+=1
+        if verbose:
+            print(f'\n Overall RMSE:')
+            print(f'RMSE Mean: {np.mean(rmse)}   Std: {np.std(rmse)}')
+        return np.mean(rmse)
+    #mae
+  def mae(self, reg,verbose=True):
+        mae = []
+        i=0
+        for tr, te in self.kf.split(self.X,self.y):
+            reg.fit(self.X[tr],self.y[tr])
+            y_pred=reg.predict(self.X[te])
+            mae.append(mean_absolute_error(y_pred,self.y[te]))
+            if verbose:
+                print('-'*30)
+                print(f'\nFold {i+1} out of {self.n_splits}')
+                print(f'MAE: {mae[i]}')
+            i+=1
+        if verbose:
+            print(f'\n Overall MAE:')
+            print(f'MAE Mean: {np.mean(mae)}   Std: {np.std(mae)}')
+        return np.mean(mae)
+    #precision score
+  
