2ece141535d9a1c38cfae026601eb640fa81d3e2
Alefe <alefeofdealmeida@gmail.com>
2019-12-30 12:07:40 -0200

    ML_utils.py update: inserting a function to perform gridsearch

--- ML_utils.py
+++ ML_utils.py
@@ -237,3 +237,23 @@ def feature_importance_plot(algorithm,X_train,y_train,of_type):
         plt.xlabel("features")
         plt.xticks(rotation='vertical')
         plt.title('FEATURE IMPORTANCES')
+
+def algorithm_grid_search_cv(X_train_data, X_test_data, y_train_data, y_test_data, 
+                       model, param_grid, cv=10, scoring_fit='neg_mean_squared_error',
+                       do_probabilities = False):
+    gs = GridSearchCV(
+        estimator=model,
+        param_grid=param_grid, 
+        cv=cv, 
+        n_jobs=-1, 
+        scoring=scoring_fit,
+        verbose=2
+    )
+    fitted_model = gs.fit(X_train_data, y_train_data)
+    
+    if do_probabilities:
+      pred = fitted_model.predict_proba(X_test_data)
+    else:
+      pred = fitted_model.predict(X_test_data)
+    
+    return fitted_model, pred
